0e7ca12fdecccf0d143745c9c1b2d25ed9ac3183
Norbert Śleziak <norr.bitt@gmail.com>
2024-03-05 20:51:13 +0100

    Division code into methods - Command class

--- src/main/java/pl/norbit/treecuter/commands/TreeCuterCommand.java
+++ src/main/java/pl/norbit/treecuter/commands/TreeCuterCommand.java
@@ -3,86 +3,105 @@ package pl.norbit.treecuter.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import pl.norbit.treecuter.config.Settings;
-import pl.norbit.treecuter.item.ItemService;
+import pl.norbit.treecuter.utils.item.ItemsUtils;
 import pl.norbit.treecuter.service.ToggleService;
 import pl.norbit.treecuter.utils.ChatUtils;
+import pl.norbit.treecuter.utils.PermissionsUtils;
 
-public class TreeCuterCommand implements CommandExecutor {
+import java.util.List;
+
+public class TreeCuterCommand implements CommandExecutor, TabCompleter {
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-
         if(args.length == 0) {
             sendInfo(sender);
             return true;
         }
+        String arg = args[0].toUpperCase();
 
-        if(args[0].equalsIgnoreCase("reload")){
+        switch (arg) {
+            case "RELOAD" -> {
+                if(hasNotPermission(sender, "reload")) return true;
 
-            if(!sender.hasPermission("treecuter.reload")){
-                sender.sendMessage(ChatUtils.format(Settings.PERMISSION_MESSAGE));
+                sender.sendMessage(ChatUtils.format(Settings.RELOAD_START));
+                Settings.loadConfig(true);
+                sender.sendMessage(ChatUtils.format(Settings.RELOAD_END));
                 return true;
             }
+            case "GET" -> {
+                if(hasNotPermission(sender, "get")) return true;
 
-            sender.sendMessage(ChatUtils.format(Settings.RELOAD_START));
-            Settings.loadConfig(true);
-            sender.sendMessage(ChatUtils.format(Settings.RELOAD_END));
-            return true;
-        }else if(args[0].equalsIgnoreCase("get")){
+                if(!Settings.TOOL_ENABLE) {
+                    sender.sendMessage(ChatUtils.format(Settings.TOOL_DISABLED));
+                    return true;
+                }
+                var p = getPlayer(sender);
 
-            if(!sender.hasPermission("treecuter.get")){
-                sender.sendMessage(ChatUtils.format(Settings.PERMISSION_MESSAGE));
+                if(p == null) return true;
+
+                var item = ItemsUtils.getItem();
+
+                p.getInventory().addItem(item);
+
+                p.sendMessage(ChatUtils.format(Settings.TOOL_GET));
                 return true;
             }
+            case "TOGGLE" -> {
+                if (hasNotPermission(sender, "toggle")) return true;
 
-            if(!Settings.TOOL_ENABLE) {
-                sender.sendMessage(ChatUtils.format(Settings.TOOL_DISABLED));
+                var p = getPlayer(sender);
+
+                if (p == null) return true;
+
+                boolean status = ToggleService.changeToggle(p.getUniqueId());
+
+                String message = status ? Settings.TOGGLE_MESSAGE_ON : Settings.TOGGLE_MESSAGE_OFF;
+
+                p.sendMessage(ChatUtils.format(message));
                 return true;
             }
-
-            if(!(sender instanceof Player player)){
-                sender.sendMessage(ChatUtils.format(Settings.CONSOLE_MESSAGE));
+            default -> {
+                sendInfo(sender);
                 return true;
             }
+        }
+    }
 
-            var item = ItemService.getItem();
+    private boolean hasNotPermission(CommandSender sender, String permission){
+        String perm = "treecuter." + permission;
 
-            player.getInventory().addItem(item);
-
-            player.sendMessage(ChatUtils.format(Settings.TOOL_GET));
-            return true;
-        }else if(args[0].equalsIgnoreCase("toggle")){
-
-            if(!sender.hasPermission("treecuter.toggle")){
-                sender.sendMessage(ChatUtils.format(Settings.PERMISSION_MESSAGE));
-                return true;
-            }
-
-            if(!(sender instanceof Player player)){
-                sender.sendMessage(ChatUtils.format(Settings.CONSOLE_MESSAGE));
-                return true;
-            }
-
-            boolean status = ToggleService.changeToggle(player.getUniqueId());
-
-            if(status) player.sendMessage(ChatUtils.format(Settings.TOGGLE_MESSAGE_ON));
-            else player.sendMessage(ChatUtils.format(Settings.TOGGLE_MESSAGE_OFF));
+        if(!PermissionsUtils.hasPermission(sender, perm)){
+            sender.sendMessage(ChatUtils.format(Settings.PERMISSION_MESSAGE));
             return true;
         }
+        return false;
+    }
 
-        sendInfo(sender);
-        return true;
+    private Player getPlayer(CommandSender sender){
+        if((sender instanceof Player player)) {
+            return player;
+        }
+        sender.sendMessage(ChatUtils.format(Settings.CONSOLE_MESSAGE));
+        return null;
     }
 
     private static void sendInfo(CommandSender sender){
-
         if(!sender.hasPermission("treecuter.help")){
             sender.sendMessage(ChatUtils.format(Settings.PERMISSION_MESSAGE));
             return;
         }
 
         Settings.HELP_MESSAGE.stream().map(ChatUtils::format).forEach(sender::sendMessage);
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return List.of("reload", "get", "toggle");
     }
 }
