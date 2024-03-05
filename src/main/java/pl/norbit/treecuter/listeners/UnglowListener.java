532e0edc0a5f25ca141550e30a7e921827ba5c47
Norbert Śleziak <norr.bitt@gmail.com>
2024-03-05 20:46:23 +0100

    Move class to other package

--- src/main/java/pl/norbit/treecuter/listeners/UnglowListener.java
+++ src/main/java/pl/norbit/treecuter/listeners/UnglowListener.java
@@ -1,35 +1,34 @@
-package pl.norbit.treecuter.glow;
+package pl.norbit.treecuter.listeners;
 
+import org.bukkit.Server;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPhysicsEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import pl.norbit.treecuter.TreeCuter;
+import pl.norbit.treecuter.utils.GlowUtils;
 import pl.norbit.treecuter.utils.TaskUtils;
 
 import java.util.Collection;
 
 public class UnglowListener implements Listener {
+    private final Server SERVER = TreeCuter.getInstance().getServer();
 
     private void unsetGlow(Block b){
         TaskUtils.runTaskLaterAsynchronously(() -> {
-            Collection<? extends Player> onlinePlayers = TreeCuter.getInstance().getServer().getOnlinePlayers();
+            Collection<? extends Player> onlinePlayers = SERVER.getOnlinePlayers();
 
-            onlinePlayers.forEach(p -> GlowingService.unsetGlowing(b, p));
+            onlinePlayers.forEach(p -> GlowUtils.unsetGlowing(b, p));
         }, 0L);
     }
 
     @EventHandler
-    public void onBlockPhysics(BlockPhysicsEvent e) {
-        unsetGlow(e.getBlock());
-    }
-    @EventHandler
     public void onBlockBreak(BlockBreakEvent e) {
         unsetGlow(e.getBlock());
     }
+
     @EventHandler
     public void onEntityExplode(EntityExplodeEvent e) {
          e.blockList().forEach(this::unsetGlow);
