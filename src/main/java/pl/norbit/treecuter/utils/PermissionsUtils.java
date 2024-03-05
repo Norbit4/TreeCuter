dbe4f2b0c550cd0a3bf5145c8b89bf5dcfcea090
Norbert Śleziak <norr.bitt@gmail.com>
2024-03-05 20:34:31 +0100

    Remove old methods

---
+++ src/main/java/pl/norbit/treecuter/utils/PermissionsUtils.java
@@ -0,0 +1,29 @@
+package pl.norbit.treecuter.utils;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+
+public class PermissionsUtils {
+
+    private PermissionsUtils() {
+        throw new IllegalStateException("This class cannot be instantiated");
+    }
+
+    public static boolean hasPermission(Player p, String perm) {
+        if(p.isOp()) return true;
+
+        if(p.hasPermission("*")) return true;
+
+        return p.hasPermission(perm);
+    }
+
+    public static boolean hasPermission(CommandSender sender, String perm) {
+        if(sender.isOp()) return true;
+
+        if(sender.hasPermission("*")) return true;
+
+        return sender.hasPermission(perm);
+    }
+
+}
