e40b63cf58b475c210d32fa69b79b014460faae8
Norbert Śleziak <norr.bitt@gmail.com>
2024-06-28 18:07:01 +0200

    update settings

--- src/main/java/pl/norbit/treecuter/config/Settings.java
+++ src/main/java/pl/norbit/treecuter/config/Settings.java
@@ -1,5 +1,7 @@
 package pl.norbit.treecuter.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import pl.norbit.treecuter.TreeCuter;
@@ -8,22 +10,110 @@ import pl.norbit.treecuter.service.TreePlanterService;
 import java.util.*;
 
 public class Settings {
-    public static int DEFAULT_EFFECT_LEVEL,  MAX_BLOCKS, MIN_BLOCKS;
-    public static boolean SHIFT_MINING, APPLY_MINING_EFFECT, ITEMS_TO_INVENTORY, WORLDGUARD_IS_ENABLED,
-            ITEMS_ADDER_IS_ENABLED, AUTO_PLANT, USE_PERMISSIONS, GLOWING_BLOCKS;
-    public static List<Material> ACCEPT_TOOLS, ACCEPT_WOOD_BLOCKS, AUTO_PLANT_SAPLINGS;
-    public static List<String> BLOCK_WORLDS;
-    public static String PERMISSION;
-    public static ChatColor GLOWING_COLOR;
-    public static String TOOL_NAME;
-    public static String TOOL_MATERIAL;
-    public static boolean TOOL_ENABLE;
-    public static String PERMISSION_MESSAGE, TOGGLE_MESSAGE_ON, TOGGLE_MESSAGE_OFF, CONSOLE_MESSAGE, RELOAD_START,
-            RELOAD_END, TOOL_GET,TOOL_DISABLED;
-    public static List<String> HELP_MESSAGE;
+
+    @Getter
+    private static int defaultEffectLevel;
+
+    //block limits
+    @Getter
+    private static int maxBlocks;
+    @Getter
+    private static int minBlocks;
+
+    //addons
+    @Getter
+    @Setter
+    private static boolean worldGuardEnabled;
+    @Getter
+    @Setter
+    private static boolean itemsAdderEnabled;
+
+    //settings
+    @Getter
+    private static boolean shiftMining;
+    @Getter
+    private static boolean applyMiningEffect;
+    @Getter
+    private static boolean itemsToInventory;
+    @Getter
+    private static boolean glowingBlocks;
+    private static boolean autoPlant;
+
+    //accepted blocks
+    private static List<Material> acceptTools;
+    private static List<Material> acceptWoodBlocks;
+    private static List<Material> autoPlantSapling;
+
+    //block worlds
+    private static List<String> blockWorlds;
+
+    //permission
+    @Getter
+    private static boolean usePermissions;
+
+    @Getter
+    private static String permission;
+
+    @Getter
+    private static ChatColor glowingColor;
+
+    //custom tool
+    @Getter
+    private static String toolName;
+    @Getter
+    private static String toolMaterial;
+    @Getter
+    private static boolean toolEnable;
+
+    //messages
+    @Getter
+    private static String permissionMessage;
+
+    @Getter
+    private static String toggleMessageOn;
+    @Getter
+    private static String toggleMessageOff;
+
+    @Getter
+    private static String consoleMessage;
+
+    @Getter
+    private static String reloadStart;
+    @Getter
+    private static String reloadEnd;
+
+    @Getter
+    private static String toolGet;
+    @Getter
+    private static String toolDisabled;
+
+    @Getter
+    private static List<String> helpMessage;
+
+    private static List<Material> groundBlocks;
 
     private Settings() {
         throw new IllegalStateException("This class cannot be instantiated");
+    }
+
+    public static boolean isAcceptedSapling(Material type){
+        return autoPlantSapling.contains(type);
+    }
+
+    public static boolean isAcceptedWoodBlock(Material type){
+        return acceptWoodBlocks.contains(type);
+    }
+
+    public static boolean isAcceptedTool(Material type){
+        return acceptTools.contains(type);
+    }
+
+    public static boolean isBlockedWorld(String worldName){
+        return blockWorlds.contains(worldName);
+    }
+
+    public static boolean isGround(Material type){
+        return groundBlocks.contains(type);
     }
 
     public static void loadConfig(boolean reload) {
@@ -34,71 +124,78 @@ public class Settings {
 
         var config = javaPlugin.getConfig();
 
-        MAX_BLOCKS = config.getInt("max-blocks");
-        MIN_BLOCKS = config.getInt("min-blocks");
+        maxBlocks = config.getInt("max-blocks");
+        minBlocks = config.getInt("min-blocks");
 
-        SHIFT_MINING = config.getBoolean("shift-mining");
+        shiftMining = config.getBoolean("shift-mining");
 
-        APPLY_MINING_EFFECT = config.getBoolean("mining-effect.enable");
-        DEFAULT_EFFECT_LEVEL = config.getInt("mining-effect.default-level");
+        applyMiningEffect = config.getBoolean("mining-effect.enable");
+        defaultEffectLevel = config.getInt("mining-effect.default-level");
 
-        ITEMS_TO_INVENTORY = config.getBoolean("items-to-inventory");
+        itemsToInventory = config.getBoolean("items-to-inventory");
 
-        AUTO_PLANT = config.getBoolean("auto-plant");
-        USE_PERMISSIONS = config.getBoolean("use-permissions");
-        PERMISSION = config.getString("permission");
+        autoPlant = config.getBoolean("auto-plant");
+        usePermissions = config.getBoolean("use-permissions");
+        permission = config.getString("permission");
 
-        BLOCK_WORLDS = config.getStringList("block-worlds");
+        blockWorlds = config.getStringList("block-worlds");
 
-        TOOL_ENABLE = config.getBoolean("custom-tool.enable");
-        TOOL_NAME = config.getString("custom-tool.name");
+        toolEnable = config.getBoolean("custom-tool.enable");
+        toolName = config.getString("custom-tool.name");
 
-        TOOL_MATERIAL = config.getString("custom-tool.material");
+        toolMaterial = config.getString("custom-tool.material");
 
-        ACCEPT_TOOLS = new ArrayList<>();
-        ACCEPT_WOOD_BLOCKS = new ArrayList<>();
-        AUTO_PLANT_SAPLINGS = new ArrayList<>();
+        acceptTools = new ArrayList<>();
+        acceptWoodBlocks = new ArrayList<>();
+        autoPlantSapling = new ArrayList<>();
 
         config.getStringList("accept-tools")
                 .stream()
                 .map(Material::getMaterial)
                 .filter(Objects::nonNull)
-                .forEach(ACCEPT_TOOLS::add);
+                .forEach(acceptTools::add);
 
         config.getStringList("accept-wood-blocks")
                 .stream()
                 .map(Material::getMaterial)
                 .filter(Objects::nonNull)
-                .forEach(ACCEPT_WOOD_BLOCKS::add);
+                .forEach(acceptWoodBlocks::add);
 
         config.getStringList("auto-plant-saplings")
                 .stream()
                 .map(Material::getMaterial)
                 .filter(Objects::nonNull)
-                .forEach(AUTO_PLANT_SAPLINGS::add);
+                .forEach(autoPlantSapling::add);
 
-        GLOWING_BLOCKS = config.getBoolean("glowing-blocks");
+        glowingBlocks = config.getBoolean("glowing-blocks");
 
         String color = config.getString("glowing-color");
 
         try {
-            GLOWING_COLOR = ChatColor.valueOf(color.toUpperCase());
+            glowingColor = ChatColor.valueOf(color.toUpperCase());
         }catch (IllegalArgumentException e){
             TreeCuter.getInstance().getLogger().warning("Wrong glowing color: " + color);
-            GLOWING_COLOR = ChatColor.RED;
+            glowingColor = ChatColor.RED;
         }
 
-        PERMISSION_MESSAGE = config.getString("messages.permission");
-        TOGGLE_MESSAGE_ON = config.getString("messages.toggle.enable");
-        TOGGLE_MESSAGE_OFF = config.getString("messages.toggle.disable");
-        CONSOLE_MESSAGE = config.getString("messages.console");
-        RELOAD_START = config.getString("messages.reload.start");
-        RELOAD_END = config.getString("messages.reload.end");
-        TOOL_GET = config.getString("messages.tool.get");
-        TOOL_DISABLED = config.getString("messages.tool.disabled");
-        HELP_MESSAGE = config.getStringList("messages.help");
+        permissionMessage = config.getString("messages.permission");
+        toggleMessageOn = config.getString("messages.toggle.enable");
+        toggleMessageOff = config.getString("messages.toggle.disable");
+        consoleMessage = config.getString("messages.console");
+        reloadStart = config.getString("messages.reload.start");
+        reloadEnd = config.getString("messages.reload.end");
+        toolGet = config.getString("messages.tool.get");
+        toolDisabled = config.getString("messages.tool.disabled");
+        helpMessage = config.getStringList("messages.help");
 
-        if(AUTO_PLANT) TreePlanterService.start();
+        groundBlocks = config.getStringList("sapling-ground-materials")
+                .stream()
+                .map(String::toUpperCase)
+                .map(Material::getMaterial)
+                .filter(Objects::nonNull)
+                .toList();
+
+        if(autoPlant) TreePlanterService.start();
         else TreePlanterService.stop();
     }
 }
