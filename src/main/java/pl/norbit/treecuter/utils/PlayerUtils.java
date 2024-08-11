e92f551026a9ad2e440a8872a537e0b401d4cf4b
Norbert Śleziak <norr.bitt@gmail.com>
2024-08-11 21:42:15 +0200

    Update PlayerUtils.java

--- src/main/java/pl/norbit/treecuter/utils/PlayerUtils.java
+++ src/main/java/pl/norbit/treecuter/utils/PlayerUtils.java
@@ -5,6 +5,7 @@ import pl.norbit.treecuter.TreeCuter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class PlayerUtils {
 
@@ -14,5 +15,9 @@ public class PlayerUtils {
 
     public static List<Player> getOnlinePlayers(){
         return new ArrayList<>(TreeCuter.getInstance().getServer().getOnlinePlayers());
+    }
+
+    public static Player getPlayer(UUID playerUUID){
+        return TreeCuter.getInstance().getServer().getPlayer(playerUUID);
     }
 }
