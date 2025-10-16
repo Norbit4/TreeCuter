e467dbcf278c914a7c76cfa52422dc75537068e7
Norbert Śleziak <46154743+Norbit4@users.noreply.github.com>
2025-10-16 22:44:56 +0200

    Add glowing to latest

--- src/main/java/pl/norbit/treecuter/model/GlowBlock.java
+++ src/main/java/pl/norbit/treecuter/model/GlowBlock.java
@@ -4,10 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.block.Block;
-import org.bukkit.block.Lidded;
 
 import java.util.List;
-import java.util.Set;
 
 @Getter
 @AllArgsConstructor
